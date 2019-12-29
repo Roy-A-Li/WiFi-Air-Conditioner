ccaa24db16284b92b815a14fd89dd054094d70ad
Roy Li <royl.usa@gmail.com>
2019-12-29 05:07:42 -0800

    Added desktop application

---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System;
+using System.IO;
+using System.Net;
+
+namespace Air_Conditioner_Control
+{
+    public partial class Form1 : Form
+    {
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        public static int toggle = 0;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,
+                         int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void incTemp_Click(object sender, EventArgs e)
+        {
+            WebRequest request = WebRequest.Create("http://acserver/tempInc"); // Replace with static web server IP
+            WebResponse response = request.GetResponse();
+            response.Close();
+        }
+
+        private void decTemp_Click(object sender, EventArgs e)
+        {
+            WebRequest request = WebRequest.Create("http://acserver/tempDec");
+            WebResponse response = request.GetResponse();
+            response.Close();
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void textBox1_TextChanged_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void power_Click(object sender, EventArgs e)
+        {
+            WebRequest request;
+            WebResponse response;
+
+            if (toggle == 0)
+            {
+                request = WebRequest.Create("http://acserver/relayOn");
+                response = request.GetResponse();
+                response.Close();
+                toggle++;
+            } else
+            {
+                request = WebRequest.Create("http://acserver/relayOff");
+                response = request.GetResponse();
+                response.Close();
+                toggle--;
+            }
+        }
+    }
+}
